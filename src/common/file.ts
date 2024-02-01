f707de3e649f0f8472450decbcd257498869dd3f
msojocs <jiyecafe@gmail.com>
2024-02-01 13:17:36 +0800

    feat: 图片文件信息解析

---
+++ src/common/file.ts
@@ -0,0 +1,43 @@
+import fs from 'fs'
+import crypto from 'crypto'
+import imageSize from 'image-size'
+import { ImageInfo } from './interface/file'
+
+/**
+ * 获取图像信息
+ * @param path 图片路径
+ * @returns 取图像信息
+ */
+export const getImageInfo = async (path: string): Promise<ImageInfo | undefined> => {
+  if (!fs.existsSync(path)) return undefined
+  const ret = imageSize(path)
+  const md5 = await getFileMd5(path)
+  const stat = fs.statSync(path)
+  return {
+    width: ret.width || 0,
+    height: ret.width || 0,
+    md5: md5,
+    size: stat.size
+  }
+}
+
+/**
+ * 计算文件md5
+ * @param path 文件路径
+ */
+export const getFileMd5 = (path: string) => {
+  return new Promise<string>((resolve, reject) => {
+    const stream = fs.createReadStream(path);
+    const hash = crypto.createHash('md5');
+    stream.on('data', chunk => {
+      if (typeof chunk == 'string')
+        hash.update(chunk, 'utf8');
+      else 
+        hash.update(chunk.toString(), 'utf8');
+    });
+    stream.on('end', () => {
+      const md5 = hash.digest('hex');
+      resolve(md5)
+    });
+  })
+}
---
+++ src/common/interface/file.d.ts
@@ -0,0 +1,9 @@
+/**
+ * 图像信息
+ */
+export interface ImageInfo {
+  width: number
+  height: number
+  size: number
+  md5: string
+}
