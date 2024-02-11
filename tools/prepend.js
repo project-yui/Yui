de8e3bf192a4b50589033467a23bca9c259d8a03
msojocs <jiyecafe@gmail.com>
2024-02-11 17:34:32 +0800

    refactor: 移除electron

--- tools/prepend.js
+++ tools/prepend.js
@@ -8,28 +8,29 @@
       /**  @type {import('fs')} */
       const fs = orgi_rquire('fs')
       const path = orgi_rquire('path')
-      /**  @type {import('electron')} */
-      const electron = orgi_rquire('electron')
-      const appPath = electron.app.getAppPath()
-      log('appPath:', appPath)
-      const packagePath = path.resolve(appPath, './package.json')
+      const packagePath = path.resolve(__dirname, '../package.json')
       if (!fs.existsSync(packagePath)) {
         log('config not exists!')
         process.exit(1)
       }
       const config = JSON.parse(fs.readFileSync(packagePath).toString())
       const version = config.version
-      const wrapperPath = path.resolve(appPath, `./versions/${version}/wrapper.node`)
-      if (!fs.existsSync(wrapperPath)) {
-        log('wrapper.node not exists!')
-        const zipPath = path.resolve(appPath, `./versions/${version}.zip`)
-        log('try to extract zip file:', zipPath)
-        /**  @type {import('adm-zip')} */
-        const AdmZip = require('./adm-zip')
-        const admzip = new AdmZip.admZip.default(zipPath);
-        admzip.extractAllTo(path.resolve(appPath, `./versions/${version}`)); // 把整个压缩包完全解压
+      let wrapperPath = path.resolve(__dirname, `../versions/${version}/wrapper.node`)
+      if (process.platform === 'win32') {
+        if (!fs.existsSync(wrapperPath)) {
+          log('wrapper.node not exists!')
+          const zipPath = path.resolve(__dirname, `../versions/${version}.zip`)
+          log('try to extract zip file:', zipPath)
+          /**  @type {import('adm-zip')} */
+          const AdmZip = require('./adm-zip')
+          const admzip = new AdmZip.admZip.default(zipPath);
+          admzip.extractAllTo(path.resolve(__dirname, `../versions/${version}`)); // 把整个压缩包完全解压
+        }
       }
-      return orgi_rquire.apply(this, [path.resolve(appPath, `./versions/${version}/wrapper.node`)])
+      else {
+        wrapperPath = path.resolve(__dirname, `../wrapper.node`)
+      }
+      return orgi_rquire.apply(this, [wrapperPath])
     }
     return orgi_rquire.apply(this, args)
   }
