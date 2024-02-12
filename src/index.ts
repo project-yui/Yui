f4f5d7c3029743ad0eb3a0e2bedb973d71a8635e
msojocs <jiyecafe@gmail.com>
2024-02-12 15:45:13 +0800

    docs: 补充了一些注释说明

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,8 @@ switch (process.env['YUKIHANA_ACTION']) {
       require('./compile.js')
     break
   default: {
+      // 不能删除，否则bytenode会被rollup移除
+      // bytenode内部添加了对.jsc文件加载的支持
       bytenode
       // 从字节码启动
       require('./core.jsc')
