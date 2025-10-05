7b8f22bc90e6f7cdfe4f65161c82378fa7f4d6be
msojocs <jiyecafe@gmail.com>
2025-10-05 18:53:16 +0800

    fix: package version

--- src/ntqq/common/utils.ts
+++ src/ntqq/common/utils.ts
@@ -4,11 +4,14 @@ import path from "path"
 import { CustomError } from "../../server/error/custom-error"
 
 export const getNTPackageInfo = (): NTPackageInfo => {
-  const pkgPath = path.resolve(__dirname, '../package.json')
+  const pkgPath = path.resolve(__dirname, './package.json')
   if (!existsSync(pkgPath)) {
     throw new CustomError(500, 'Can not find package.json!')
   }
   const pkg = JSON.parse(readFileSync(pkgPath).toString())
+  if (!pkg.version) {
+    throw new CustomError(500, 'Invalid package.json!')
+  }
   return {
     version: pkg.version
   }
